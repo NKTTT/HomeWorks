6255c385808ad5f23f56328505bfb670a356e654
NKTTT <84173688+NKTTT@users.noreply.github.com>
2021-06-23 21:43:52 +0300

    Add files via upload

---
+++ Homework2/src/main/java/HomeWork/Homework2/src/main/java/HomeWork/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package HomeWork;
+
+import HomeWork.CustomException;
+
+public class MyArrayDataException extends CustomException {
+
+    public MyArrayDataException(int row, int col) {
+        super(String.format("Parse to int exception in array[%d, %d]", row, col));
+    }
+}
